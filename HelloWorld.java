fa021e63b9d81d1f6ffefc820f21b225d51c9361
srinivasapsit <47416519+srinivasapsit@users.noreply.github.com>
2021-03-24 18:17:07 +0530

    Update and rename helloWorld.java to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-class helloWorld{
+class HelloWorld{
 public static void main(String args[]){
 System.out.println("Hello World");
 }
   public static void add(int num1,int num2){
     System.out.println(num1+num2);
+}
 }
